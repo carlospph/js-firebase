d27a780897e05529461995bc40f27a8d80e8f8db
Carlos Nascimento <80932010+carlospph@users.noreply.github.com>
2026-01-10 22:20:08 -0300

    .

--- js/index.js
+++ js/index.js
@@ -27,16 +27,15 @@ function acessarHome() {
     //     });
 
 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+   firebase.auth().signInWithEmailAndPassword(email, password)
   .then((userCredential) => {
-    // Signed in 
+    // Signed in
     var user = userCredential.user;
     // ...
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-    // ..
   });
 
 
