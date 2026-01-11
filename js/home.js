41d753a18ae53c72a1e50c40b9bb9f7dc79d3a06
Carlos Nascimento <80932010+carlospph@users.noreply.github.com>
2026-01-11 11:40:53 -0300

    separar html e js do home

---
+++ js/home.js
@@ -0,0 +1,22 @@
+import { auth, db } from "../js/firebase.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+// import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+       //chamar a função deslogar ao clicar
+       
+    } else {
+        window.location.href = "../index.html";
+    }
+});
+
+
+//Função deslogar 
+function deslogar() {
+    signOut(auth).then(() => {
+        window.location.href = "../index.html";
+    }).catch((error) => {
+        alert("Erro ao deslogar: " + error.message);
+    });
+}
