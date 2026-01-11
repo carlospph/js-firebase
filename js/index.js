581f8a64d72e571d74248bd205cbfc11ba5a5b15
Carlos Nascimento <80932010+carlospph@users.noreply.github.com>
2026-01-10 22:06:30 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-import { auth } from './firebase.js';
-// 1. Mudamos a função importada para signIn...
+import { auth } from './firebase.js'; // Conexao do firebase
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 const btnLogin = document.getElementById("btnLogin");
@@ -12,23 +11,32 @@ function acessarHome() {
         return;
     }
 
-    signInWithEmailAndPassword(auth, inputLogin, inputSenha)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log("Login realizado com sucesso!");
-            window.location.href = "html/home.html";
-        })
-        .catch((error) => {
-            console.error("Erro no Firebase:", error.code);
+    // signInWithEmailAndPassword(auth, inputLogin, inputSenha)
+    //     .then((userCredential) => {
+    //         const user = userCredential.user;
+    //         window.location.href = "html/home.html";
+    //     })
+    //     .catch((error) => {
+    //         if (error.code === "auth/invalid-credential") {
+    //             alert("E-mail ou senha incorretos.");
+    //         } else if (error.code === "auth/user-not-found") {
+    //             alert("Usuário não encontrado.");
+    //         } else {
+    //             alert("Erro ao tentar entrar: " + error.message);
+    //         }
+    //     });
 
-            if (error.code === "auth/invalid-credential") {
-                alert("E-mail ou senha incorretos.");
-            } else if (error.code === "auth/user-not-found") {
-                alert("Usuário não encontrado.");
-            } else {
-                alert("Erro ao tentar entrar: " + error.message);
-            }
-        });
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            var email = user.email;
+        } else {
+            
+        }
+    });
+    firebase.auth().signInWithEmailAndPassword(email, password);
+
+
 }
 
 btnLogin.addEventListener("click", acessarHome);
